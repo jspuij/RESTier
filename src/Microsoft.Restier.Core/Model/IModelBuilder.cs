6d888bbc75736b41945aa60f7291a8fa0e91fc42
Jan-Willem Spuij <jan-willem.spuij@nocore.nl>
2025-04-25 17:57:36 +0200

    Modelbuilder interface again and chained service

---
+++ src/Microsoft.Restier.Core/Model/IModelBuilder.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License.  See License.txt in the project root for license information.
+
+using Microsoft.OData.Edm;
+using Microsoft.Restier.Core.DependencyInjection;
+
+namespace Microsoft.Restier.Core.Model
+{
+    /// <summary>
+    /// The service for model generation.
+    /// </summary>
+    public interface IModelBuilder : IChainedService<IModelBuilder>
+    {
+        /// <summary>
+        /// Asynchronously gets an API model for an API.
+        /// </summary>
+        /// <returns>
+        /// A task that represents the asynchronous
+        /// operation whose result is the API model.
+        /// </returns>
+        IEdmModel GetEdmModel();
+
+    }
+}

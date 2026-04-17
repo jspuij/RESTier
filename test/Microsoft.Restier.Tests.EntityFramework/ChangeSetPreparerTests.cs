927864327f32d59c539a9c3e45eb2a0230c420d9
Jan-Willem Spuij <jan-willem.spuij@nocore.nl>
2026-04-17 10:20:24 +0200

    refactor: convert ChangeSetPreparerTests from MSTest to xUnit v3

--- test/Microsoft.Restier.Tests.EntityFramework/ChangeSetPreparerTests.cs
+++ test/Microsoft.Restier.Tests.EntityFramework/ChangeSetPreparerTests.cs
@@ -1,57 +1,60 @@
-﻿// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License.  See License.txt in the project root for license information.
 
 using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Restier.Breakdance;
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Restier.Breakdance;
 using Microsoft.Restier.Core;
 using Microsoft.Restier.Core.Submit;
 using Microsoft.Restier.Tests.Shared;
 using Microsoft.Restier.Tests.Shared.Scenarios.Library;
+using Xunit;
+
+#if EFCore
+using Microsoft.Restier.EntityFrameworkCore;
+using Microsoft.Restier.Tests.Shared.Scenarios.Library.EFCore;
+
+namespace Microsoft.Restier.Tests.EntityFrameworkCore;
+#else
 using Microsoft.Restier.Tests.Shared.Scenarios.Library.EF6;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace Microsoft.Restier.EntityFramework.Tests
-{
-
-    [TestClass]
-    public class ChangeSetPreparerTests : RestierTestBase
-#if NET6_0_OR_GREATER
-        <LibraryApi>
+namespace Microsoft.Restier.Tests.EntityFramework;
 #endif
+
+public class ChangeSetPreparerTests : RestierTestBase<LibraryApi>
+{
+    [Fact]
+    public async Task ComplexTypeUpdate()
     {
-        [TestMethod]
-        public async Task ComplexTypeUpdate()
-        {
-            var provider = await RestierTestHelpers.GetTestableInjectionContainer<LibraryApi>(serviceCollection: (services) => services.AddEntityFrameworkServices<LibraryContext>());
-            provider.Should().NotBeNull();
-            
-            var api = provider.GetTestableApiInstance<LibraryApi>();
-            api.Should().NotBeNull();
+        var provider = await RestierTestHelpers.GetTestableInjectionContainer<LibraryApi>(
+            serviceCollection: services => services.AddEntityFrameworkServices<LibraryContext>());
+        provider.Should().NotBeNull();
 
-            var item = new DataModificationItem(
-                "Readers",
-                typeof(Employee),
-                null,
-                RestierEntitySetOperation.Update, 
-                new Dictionary<string, object> { { "Id", new Guid("53162782-EA1B-4712-AF26-8AA1D2AC0461") } },
-                new Dictionary<string, object>(),
-                new Dictionary<string, object> { { "Addr", new Dictionary<string, object> { { "Zip", "332" } } } });
-            var changeSet = new ChangeSet(new[] { item });
-            var sc = new SubmitContext(api, changeSet);
+        var api = provider.GetTestableApiInstance<LibraryApi>();
+        api.Should().NotBeNull();
 
-            var changeSetPreparer = api.GetApiService<IChangeSetInitializer>();
-            changeSetPreparer.Should().NotBeNull();
+        var item = new DataModificationItem(
+            "Readers",
+            typeof(Employee),
+            null,
+            RestierEntitySetOperation.Update,
+            new Dictionary<string, object> { { "Id", new Guid("53162782-EA1B-4712-AF26-8AA1D2AC0461") } },
+            new Dictionary<string, object>(),
+            new Dictionary<string, object> { { "Addr", new Dictionary<string, object> { { "Zip", "332" } } } });
+        var changeSet = new ChangeSet(new[] { item });
+        var sc = new SubmitContext(api, changeSet);
 
-            await changeSetPreparer.InitializeAsync(sc, CancellationToken.None).ConfigureAwait(false);
-            var person = item.Resource as Employee;
+        var changeSetPreparer = provider.GetService<IChangeSetInitializer>();
+        changeSetPreparer.Should().NotBeNull();
 
-            person.Should().NotBeNull();
-            person.Addr.Zip.Should().Be("332");
-        }
+        await changeSetPreparer.InitializeAsync(sc, CancellationToken.None);
+        var person = item.Resource as Employee;
+
+        person.Should().NotBeNull();
+        person.Addr.Zip.Should().Be("332");
     }
 }
